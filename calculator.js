5473ece029177bc45d1c7976cedacb9a345a801d
emmakate balog <34043958+emmabalog@users.noreply.github.com>
2017-12-19 14:32:19 -0800

    making commit

--- calculator.js
+++ calculator.js
@@ -19,4 +19,15 @@ function e() {
 		d("Error");
 	}
 }
-			
+
+function changeCSS(cssFile, cssLinkIndex) {
+
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
